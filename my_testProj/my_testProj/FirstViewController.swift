2dcb14e081b4d2984014c1100df986c42dfa8320
Eugeny Yushekevich <eugeny.yushkevich@gmail.com>
2018-07-24 19:50:10 +0300

    picking up an image

--- my_testProj/my_testProj/FirstViewController.swift
+++ my_testProj/my_testProj/FirstViewController.swift
@@ -9,51 +9,73 @@
 import UIKit
 import MapKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     @IBOutlet weak var map: MKMapView!
-    
-    
     @IBAction func cameraButton(_ sender: UIButton) {
         
         let alertController = UIAlertController(title: "some title", message: "some message",
                                                 preferredStyle: .actionSheet)
         
         alertController.addAction(UIAlertAction(title: "Take a picture", style: .default, handler: nil))
-        alertController.addAction(UIAlertAction(title: "Choose From Library", style: .default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Choose From Library", style: .default, handler: {
+            ( action: UIAlertAction!) in self.choosePhoto()
+        }))
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alertController, animated: true)
         
+    }
+    
+    func choosePhoto(){
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.allowsEditing = false
         
+        present(imagePicker, animated: true, completion: nil)
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        imagePicker.delegate = self
         
         let span: MKCoordinateSpan = MKCoordinateSpanMake(0.1, 0.1)
         let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(53.888381, 27.544470)
         let region = MKCoordinateRegion(center: location, span: span)
         
         map.setRegion(region, animated: true)
- 
-        var annotation = MKPointAnnotation()
+        
+        let annotation = MKPointAnnotation()
         annotation.coordinate = location
         annotation.title = "title annotation"
         annotation.subtitle = "subtitle"
         map.addAnnotation(annotation)
         
-        
-        
-    }
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+   
+    
+    
 }
 
