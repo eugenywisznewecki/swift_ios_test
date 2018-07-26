2ee65ea7697976efde4fd5c6cf657161ef41c898
Eugeny <eugeny.yushkevich@gmail.com>
2018-07-26 02:17:48 -0700

    test picker view to choose a category

--- my_testProj/my_testProj/DetailViewController.swift
+++ my_testProj/my_testProj/DetailViewController.swift
@@ -8,8 +8,39 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var categoryButton: UIButton!
+    
+    @IBAction func categoryVIewButton(_ sender: UIButton) {
+        pickerView.isHidden = false
+    }
+    
+    
+    let categories = ["Friend", "Work", "Default"]
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoryButton.setTitle(categories[row], for: .normal)
+        self.view.endEditing(true)
+        pickerView.isHidden = true
+    }
+    
+    
     @IBAction func cancelButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
